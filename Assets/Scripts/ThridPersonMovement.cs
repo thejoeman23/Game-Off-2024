3ef7920848e45c00e550e0d969be0bb3821fca64
aqua <aqua.rock.slug@gmail.com>
2024-11-16 03:53:18 -0600

    gravity and metal shader

--- Assets/Scripts/ThridPersonMovement.cs
+++ Assets/Scripts/ThridPersonMovement.cs
@@ -11,33 +11,25 @@ public class ThridPersonMovement : MonoBehaviour
 
     float turnSmoothVelocity = 1f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         controller = GetComponent<CharacterController>();
         cam = Camera.main?.transform;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-
+        var horizontal = Input.GetAxisRaw("Horizontal");
+        var vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
 
         if (direction.magnitude >= .1f)
         {
-            // the angle that our player needs to be to face the direction we're going ( * mathf.rad2deg just makes it in degrees not radians)
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            // makes the turning smoother
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-
-            // not entirely sure what this does im ngl. brakeys am i right?
+            var targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             Vector3 moveDirection = Quaternion.Euler(0, angle, 0) * Vector3.forward;
-
             transform.rotation = Quaternion.Euler(0, targetAngle, 0);
-            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+            controller.SimpleMove(moveDirection.normalized * (speed * Time.deltaTime));
         }
     }
 }
